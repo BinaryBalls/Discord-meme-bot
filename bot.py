736b2c6b3cf978102153fae96d4239543ca6167f
Parth Tiwari <tiwariparth200514@gmail.com>
2026-01-21 23:25:41 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+client = MyClient(intents=intents)
+client.run('YOUR_OWN')
